8cd19a62a7f214620d3c60ac72a56a3281c4deac
dbsplaysport <hank001@gmail.com>
2018-08-06 12:16:31 +0800

    a

--- hello/admin.py
+++ hello/admin.py
@@ -7,8 +7,8 @@ class MemberAdmin(admin.ModelAdmin): #讓後台管理可以顯示下列資料
    list_display=('id', 'mId', 'mName', 'mDepartment', 'mEmail', 'mPassword', 'mMoney')
 
 class TeamAdmin(admin.ModelAdmin): #讓後台管理可以顯示下列資料
-   list_display=('id', 'tId', 'tName', 'tMember1', 'tMember2','tdep', 'tgame1', 'tgame2',
-                 'tgame3', 'tgame4', 'tgame5', 'tgame6', 'tgame7')
+   list_display=('id', 'tId', 'tName', 'tMember1', 'tMember2','tDepartment', 'tGame1', 'tGame2',
+                 'tGame3', 'tGame4', 'tGame5', 'tGame6', 'tGame7')
 
 admin.site.register(Member, MemberAdmin) #需註冊model在此
 admin.site.register(Team, TeamAdmin) #需註冊model在此
