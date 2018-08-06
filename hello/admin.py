0df97afe209456ac2e4449860b62b97a2644138f
dbsplaysport <hank001@gmail.com>
2018-08-06 11:53:01 +0800

    a

--- hello/admin.py
+++ hello/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from hello.models import Member
+from hello.models import Team
 
 # Register your models here.
+class MemberAdmin(admin.ModelAdmin): #讓後台管理可以顯示下列資料
+   list_display=('id', 'mId', 'mName', 'mDepartment', 'mEmail', 'mPassword', 'mMoney')
+
+class TeamAdmin(admin.ModelAdmin): #讓後台管理可以顯示下列資料
+   list_display=('id', 'tId', 'tName', 'tMember1', 'tMember2','tdep', 'tgame1', 'tgame2',
+                 'tgame3', 'tgame4', 'tgame5', 'tgame6', 'tgame7')
+
+admin.site.register(Member, MemberAdmin) #需註冊model在此
+admin.site.register(Team, TeamAdmin) #需註冊model在此
